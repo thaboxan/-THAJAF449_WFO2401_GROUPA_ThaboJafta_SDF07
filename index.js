8653880c8144091b75a705a997b82662239fcc1f
ThaboCodes <113469568+thaboxan@users.noreply.github.com>
2024-02-09 08:53:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+// Importing Firebase modules from URLs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+
+// Firebase app settings
+const appSettings = {
+  databaseURL: "https://scrimba-ba9df-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+const app = initializeApp(appSettings) //App variable with arguments to give the app settings we set up 
+const database = getDatabase(app) //Database variable that uses the getDatabse and passes in app
+const shoppingListInDB = ref(database, "shoppingList") //The reference 
+
+// Get element
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list") //Add shopping list from html to js
+
+// add an event listener 
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+
+    clearInputFieldEl()
+})
+
+// call onValue function so we can use realtime snapshot
+onValue(shoppingListInDB, function(snapshot) {
+
+    if (snapshot.exists()) { //this fixes our bug which prevents the last item from being removed
+        let itemsArray = Object.entries(snapshot.val())
+        
+        clearShoppingListEl()
+        //for loop to show each item
+        for (let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+            let currentItemID = currentItem[0]
+            let currentItemValue = currentItem[1]
+            
+            appendItemToShoppingListEl(currentItem)
+        }
+    } else {
+        shoppingListEl.innerHTML = "No items yet... tell me what you need!"
+    }
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+//add function to take in item and set value to empty string
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(item) {
+    let itemID = item[0]
+    let itemValue = item[1]
+    
+    let newEl = document.createElement("li")
+    
+    newEl.textContent = itemValue
+
+    newEl.addEventListener("click", function() { 
+        let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`) //to locate the item in question
+        remove(exactLocationOfItemInDB) //remove function to remove items from the database
+    })
+
+    shoppingListEl.append(newEl)
+}
